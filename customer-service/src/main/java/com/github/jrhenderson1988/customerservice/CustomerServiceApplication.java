8650086ce06bdd91eedb11070806dbd7b162b740
Jonathon Henderson <jrhenderson1988@hotmail.com>
2019-10-30 16:11:37 +0000

    Eureka integration with discovery service in order and customer services

--- customer-service/src/main/java/com/github/jrhenderson1988/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/github/jrhenderson1988/customerservice/CustomerServiceApplication.java
@@ -2,12 +2,12 @@ package com.github.jrhenderson1988.customerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CustomerServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerServiceApplication.class, args);
 	}
-
 }
