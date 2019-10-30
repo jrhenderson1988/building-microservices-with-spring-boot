2336a9aaa06551ae50067b23d3b520cb9923f718
Jonathon Henderson <jrhenderson1988@hotmail.com>
2019-10-30 15:39:45 +0000

    Add discovery service and base gradle project

---
+++ discovery-service/src/test/java/com/github/jrhenderson1988/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jrhenderson1988.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
